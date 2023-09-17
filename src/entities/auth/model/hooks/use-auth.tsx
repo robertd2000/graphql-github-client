d1f09d7276a447dba7e1b7e8181fa869b2289636
Rob <rob@example.com>
2023-09-17 11:43:33 +0300

    deploy

--- src/entities/auth/model/hooks/use-auth.tsx
+++ src/entities/auth/model/hooks/use-auth.tsx
@@ -1,22 +1,24 @@
-import { persist } from '../../../../shared/lib/browser'
-import { CREDENTIAL_KEY } from '../../lib'
-import { UserCredential } from '../../types'
+import { persist } from "../../../../shared/lib/browser";
+import { CREDENTIAL_KEY } from "../../lib";
+import { UserCredential } from "../../types";
 
 export const useAuth = () => {
   const [viewer, setViewer] = persist.useLocalStorage<UserCredential | null>(
     CREDENTIAL_KEY,
     null
-  )
-  const isAuth = !!viewer
+  );
+  const isAuth = !!viewer;
 
   const login = (credential: UserCredential) => {
-    setViewer(credential)
-    window.location.href = `/${credential.username}`
-  }
+    console.log("credential", credential);
+
+    setViewer(credential);
+    // window.location.href = `/graphql-github-client/${credential.username}`;
+  };
 
   const logout = () => {
-    setViewer(null)
-  }
+    setViewer(null);
+  };
 
-  return { isAuth, viewer, login, logout }
-}
+  return { isAuth, viewer, login, logout };
+};
--- src/shared/env/index.ts
+++ src/shared/env/index.ts
@@ -11,33 +11,34 @@
  */
 const getEnvVar = (key: string) => {
   if (import.meta.env[key] === undefined) {
-    throw new Error(`Env variable ${key} is required`)
+    throw new Error(`Env variable ${key} is required`);
   }
-  return import.meta.env[key] || ''
-}
+
+  return import.meta.env[key] || "";
+};
 
 /** @github API entrypoint */
-export const API_URL = getEnvVar('VITE_API_URL')
+export const API_URL = getEnvVar("VITE_API_URL");
 /** @github Domain link */
-export const GITHUB_DOMAIN = getEnvVar('VITE_GITHUB_DOMAIN')
+export const GITHUB_DOMAIN = getEnvVar("VITE_GITHUB_DOMAIN");
 /** @github Main repo link */
-export const GITHUB_MAIN = getEnvVar('VITE_GITHUB_MAIN')
+export const GITHUB_MAIN = getEnvVar("VITE_GITHUB_MAIN");
 /** @github Feedback link */
-export const GITHUB_FEEDBACK = getEnvVar('VITE_GITHUB_FEEDBACK')
+export const GITHUB_FEEDBACK = getEnvVar("VITE_GITHUB_FEEDBACK");
 
 /** Режим запуска программы */
-export const NODE_ENV = getEnvVar('VITE_NODE_ENV')
+export const NODE_ENV = getEnvVar("VITE_NODE_ENV");
 /** Режим разработки */
-export const isDevEnv = NODE_ENV === 'development'
+export const isDevEnv = NODE_ENV === "development";
 /** Режим продакшена */
-export const isProdEnv = NODE_ENV === 'production'
+export const isProdEnv = NODE_ENV === "production";
 
 export const firebaseConfig = {
-  apiKey: getEnvVar('VITE_FIREBASE_apiKey'),
-  authDomain: getEnvVar('VITE_FIREBASE_authDomain'),
-  databaseURL: getEnvVar('VITE_FIREBASE_databaseURL'),
-  projectId: getEnvVar('VITE_FIREBASE_projectId'),
-  storageBucket: getEnvVar('VITE_FIREBASE_storageBucket'),
-  messagingSenderId: getEnvVar('VITE_FIREBASE_messagingSenderId'),
-  appId: getEnvVar('VITE_FIREBASE_appId'),
-}
+  apiKey: getEnvVar("VITE_FIREBASE_apiKey"),
+  authDomain: getEnvVar("VITE_FIREBASE_authDomain"),
+  databaseURL: getEnvVar("VITE_FIREBASE_databaseURL"),
+  projectId: getEnvVar("VITE_FIREBASE_projectId"),
+  storageBucket: getEnvVar("VITE_FIREBASE_storageBucket"),
+  messagingSenderId: getEnvVar("VITE_FIREBASE_messagingSenderId"),
+  appId: getEnvVar("VITE_FIREBASE_appId"),
+};
